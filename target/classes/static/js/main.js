70eb163712032f97a8487cfc628aaa1128fe662c
Ardest07 <43248351+Ardest07@users.noreply.github.com>
2019-05-29 14:26:13 +0600

    Add files via upload

---
+++ target/classes/static/js/main.js
@@ -0,0 +1,20 @@
+//remain scroll position after redirect
+$(window).scroll(function () {
+    sessionStorage.scrollTop = $(this).scrollTop();
+});
+
+$(document).ready(function () {
+    if (sessionStorage.scrollTop !== "undefined") {
+        $(window).scrollTop(sessionStorage.scrollTop);
+    }
+});
+
+<!--show password when mouse is over eye icon-->
+$(".show-password").hover(
+    function() {
+        $("#password").attr("type", "text").focus();
+    },
+    function() {
+        $("#password").attr("type", "password");
+    }
+);
